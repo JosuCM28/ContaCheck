977a86ce18f03ffd20552fe4ab2c11c486fbed91
Josue Cruz Marin <cruzjosu57@gmail.com>
2025-08-20 16:02:29 -0600

    Se agrego trusproxies en appPHP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
             'redirect' => \App\Http\Middleware\RedirectByRol::class,
+            
         ]);
+         $middleware->trustProxies('*', 255);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
